4ad56c69286b0efa0086781fe533c2704e84c025
Art Beatte <art.beatte@docusign.com>
2015-07-25 08:10:41 -0700

    Deletes unused directive.

--- directives.js
+++ directives.js
@@ -22,25 +22,4 @@ let directives = angular.module('directives', []);
     };
   }]);
 
-  directives.directive('formAutofillFix', ['$timeout', function ($timeout) {
-    return function (scope, element, attrs) {
-      element.prop('method', 'post');
-      if (attrs.ngSubmit) {
-        $timeout(function () {
-          element
-              .unbind('submit')
-              .bind('submit', function (event) {
-                event.preventDefault();
-                element
-                    .find('input, textarea, select')
-                    .trigger('input')
-                    .trigger('change')
-                    .trigger('keydown');
-                scope.$apply(attrs.ngSubmit);
-              });
-        });
-      }
-    };
-  }]);
-
 })(directives);
