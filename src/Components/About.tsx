a434326036a4c15cd3d6e386b8a484ebf113ab64
Art Beatte <abeatte@gmail.com>
2025-11-09 21:12:35 -0800

    Fixes about section page alignment

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -10,15 +10,15 @@ export default function About() {
    return (
       <Element name='about'>
          <section id="about">
-            <div className="row">
-               <div className="three columns">
-                  <img className="profile-pic" src={profilepic} alt="Art Beatte Profile Pic" />
+            <div className="row about_container">
+                  <div className="columns">
+                     <img className="profile-pic" src={profilepic} alt="Art Beatte Profile Pic" />
+                  </div>
+                  <div className="nine columns main-col">
+                     <h2>About Me</h2>
+                     <p>{bio}</p>
+                  </div>
                </div>
-               <div className="nine columns main-col">
-                  <h2>About Me</h2>
-                  <p>{bio}</p>
-               </div>
-            </div>
          </section>
       </Element>
    );
